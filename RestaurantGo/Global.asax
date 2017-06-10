a36aca5dd2c80a9cbb6c644493ea639321b73d93
juanc <juanc@JTEJEDA>
2017-06-09 21:45:30 -0400

    Add project files.

---
+++ RestaurantGo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestaurantGo.MvcApplication" Language="C#" %>
---
+++ RestaurantGo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantGo.Startup))]
+namespace RestaurantGo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
