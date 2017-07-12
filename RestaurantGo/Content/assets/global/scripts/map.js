beb876e68bfab3077f00c2f8996834db82a2617f
juanc <juanc@JTEJEDA>
2017-07-11 23:46:59 -0400

    Correcciones de los js para el API de google map

--- RestaurantGo/Content/assets/global/scripts/map.js
+++ RestaurantGo/Content/assets/global/scripts/map.js
@@ -1,113 +1,128 @@
 ﻿
 
 
-var nombre = '';
-var direccion = '';
-var ubicacion = '';
-var id_ubicacion = '';
+var nombre;
+var direccion;
+var ubicacion ;
+var id_ubicacion;
+var map;
+var marker;
+var markers;
+var posicion;
+var saludo = "hola";
+var vista;
 
-function cargarDatos(preinf) {
+var autocomplete;
+var input;
+var infowindow;
+var geocoder;
+var marker;
+var plece;
+var latlang;
 
-    nombre = preinf.split(',')[0];
-    direccion = preinf.substring(nombre.length + 2);
 
-    document.getElementById('nombre').value = nombre;
-    document.getElementById('direccion').value = direccion;
-    document.getElementById('ubicacion').value = ubicacion;
-    document.getElementById('id_ubicacion').value = id_ubicacion;
+
+    function initMap() {
     
-        }
+            vista = window.location.pathname;
+            vista = vista.split('/');
+            vista = vista[2];
+            latlang = '{ lat: 18.473807, lng: -69.913407 }';
 
-function initMap() {
 
-          var map = new google.maps.Map(document.getElementById('map'), {
-              center: {lat: 18.473807, lng:  -69.913407},
-              zoom: 18
-          });
-     
-          var input = document.getElementById('pac-input');
+                     //en esta sesion esta la localizacion por defecto del mapa
+            map = new google.maps.Map(document.getElementById('map'),
+                {
+                    center: { lat: 18.473807, lng: -69.913407 },
+              zoom: 17
+                });
 
-          var autocomplete = new google.maps.places.Autocomplete(
-              input, {placeIdOnly: true});
+
+                            // en esta sesion se configura la funcionalidad de autocompletado del buscardor
+          input = document.getElementById('pac-input');
+          autocomplete = new google.maps.places.Autocomplete(
+              input, { placeIdOnly: true }
+              );
+
           autocomplete.bindTo('bounds', map);
-
           map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-          var infowindow = new google.maps.InfoWindow();
-          var geocoder = new google.maps.Geocoder;
-          var marker = new google.maps.Marker({
+
+          infowindow = new google.maps.InfoWindow();
+          geocoder = new google.maps.Geocoder;
+          marker = new google.maps.Marker({
               map: map
           });
 
 
-          var pathname = window.location.pathname;
-          var vista = pathname.split('/');
-
-
-
-          if (vista[2] != 'Create' && vista[2] != 'Edit') {
+          if (vista != 'Create' && vista != 'Edit') {
               geocodePlaceId(geocoder, map, infowindow);
-
               return;
           }
+          if ( vista == 'Edit') {
+              geocodePlaceId(geocoder, map, infowindow);
 
-
+          }
 
           marker.addListener('click', function() {
               infowindow.open(map, marker);
+           
           });
 
-          autocomplete.addListener('place_changed', function() {
+
+          autocomplete.addListener('place_changed', function () {
+
               infowindow.close();
-            
-              var place = autocomplete.getPlace();
-         
+              place = autocomplete.getPlace();
               if (!place.place_id) {
                   return;
               }
 
 
-              var pathname = window.location.pathname;
-              var vista = pathname.split('/');
-              var placeId;
+              //var pathname = window.location.pathname;
+              //var vista = pathname.split('/');
+              //var placeId;
 
-              if (vista[2] != 'Create' && vista[2] != 'Edit') {
-                  placeId = document.getElementById('id_ubicacion').innerHTML;
-                  geocodePlaceId(geocoder, map, infowindow);
+              //if (vista[2] != 'Create' && vista[2] != 'Edit') {
+              //    placeId = document.getElementById('id_ubicacion').innerHTML;
+              //    geocodePlaceId(geocoder, map, infowindow);
 
                   
-              } else {
-                  placeId = place.place_id
-              }
+              //} else {
+              //    placeId = place.place_id
+              //}
 
-              geocoder.geocode({ 'placeId': placeId }, function (results, status) {
-          
+              geocoder.geocode({ 'placeId': place.place_id }, function (results, status) {
 
                   if (status !== 'OK') {
                       window.alert('Geocoder failed due to: ' + status);
                       return;
-                  }
-                  map.setZoom(18);
+                  } 
+                  map.setZoom(17);
                   map.setCenter(results[0].geometry.location);
-                  // Set the position of the marker using the place ID and location.
 
+                  // Set the position of the marker using the place ID and location.
                   marker.setPlace({
-                      placeId: place.placeId,
+                      placeId: place.place_id,
                       location: results[0].geometry.location
                   });
                   marker.setVisible(true);
-                  document.getElementById('place-name').textContent = place.name;
-                  document.getElementById('place-id').textContent = place.place_id;
-                  document.getElementById('place-address').textContent =
+
+                  try{
+                      document.getElementById('place-name').textContent = place.name;
+                      document.getElementById('place-id').textContent = place.place_id;
+                      document.getElementById('place-address').textContent =
                       results[0].formatted_address;
+                  }catch(err)
+                  {
+
+                  }
+                      
+
                   infowindow.setContent(document.getElementById('infowindow-content'));
                   infowindow.open(map, marker);
-                
 
-                  var preinf = place.name
-                  ubicacion = results[0].geometry.location;
-                  id_ubicacion = place.place_id;
-                  cargarDatos(preinf);
+                  cargarDatos(place, results);
 
               });
           });
@@ -117,58 +132,39 @@ function initMap() {
 // This function is called when the user clicks the UI button requesting
 // a reverse geocode.
 function geocodePlaceId(geocoder, map, infowindow) {
-    // var placeId = ChIJXaeqAtZu1moR6Q7DmBg4Jt; document.getElementById('id_ubicacion').value;
+    var placeId;
+    if (vista == 'Edit') {
+        nombre = document.getElementById('nombre').value;
+        ubicacion = document.getElementById('ubicacion').value;
+        id_ubicacion = document.getElementById('id_ubicacion').value;
+        placeId = document.getElementById('id_ubicacion').value;
 
-    nombre = document.getElementById('nombre').innerHTML;
-    var ubicacion = document.getElementById('ubicacion').innerHTML;
-    id_ubicacion = document.getElementById('id_ubicacion').innerHTML;
-    var placeId = document.getElementById('id_ubicacion').innerHTML;
+    } else {
+        nombre = document.getElementById('nombre').innerHTML;
+        ubicacion = document.getElementById('ubicacion').innerHTML;
+        id_ubicacion = document.getElementById('id_ubicacion').innerHTML;
+        placeId = document.getElementById('id_ubicacion').innerHTML;
 
-    var input = document.getElementById('pac-input');
-
-    var autocomplete = new google.maps.places.Autocomplete(
-        input, {placeIdOnly: true});
-    autocomplete.bindTo('bounds', map);
-
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    var infowindow = new google.maps.InfoWindow();
-    var geocoder = new google.maps.Geocoder;
-    var marker = new google.maps.Marker({
-        map: map
-    });
+    }
 
 
-    geocoder.geocode({ 'placeId': placeId }, function (results, status) {
+
+     geocoder.geocode({ 'placeId': placeId }, function (results, status) {
         if (status === 'OK') {
             if (results[0]) {
-                map.setZoom(18);
+                map.setZoom(17);
                 map.setCenter(results[0].geometry.location);
-                var marker = new google.maps.Marker({
+                 marker = new google.maps.Marker({
                     map: map,
                     position: results[0].geometry.location
                 });
 
-
-
                 marker.setVisible(true);
                 document.getElementById('place-name').textContent = nombre;
                 document.getElementById('place-id').textContent = id_ubicacion;
-                document.getElementById('place-address').textContent =
-                    results[0].formatted_address;
+                document.getElementById('place-address').textContent =  results[0].formatted_address;
                 infowindow.setContent(document.getElementById('infowindow-content'));
                 infowindow.open(map, marker);
-
-
-                //marker.setVisible(true);
-                //document.getElementById('place-name').textContent = 'place.name';
-                //document.getElementById('place-id').textContent = 'place.place_id';
-
-                //document.getElementById('place-name').textContent = 'nombre';
-                //document.getElementById('place-id').textContent = 'id';
-
-                //infowindow.setContent(results[0].formatted_address);
-                //infowindow.open(map, marker);
 
 
             } else {
@@ -177,78 +173,30 @@ function geocodePlaceId(geocoder, map, infowindow) {
         } else {
             window.alert('Geocoder failed due to: ' + status);
         }
-
-
   
     });
 }
 
 
-
-      // This function is called when the user clicks the UI button requesting
-// a reverse geocode.
-
-
-
-//function geocodePlaceId(geocoder, map, infowindow) {
-
-//    ubicacion = document.getElementById('ubicacion').innerHTML;
-//    var placeId = document.getElementById('id_ubicacion').innerHTML;
-//        var ubicacionParte = ubicacion.split(',');
-//        var latitud = ubicacionParte[0].split('(')[1];
-//        var longitud = ubicacionParte[1].split(')')[0];
-//        alert(latitud);
-//        alert(longitud);
-//        alert(ubicacion);
-
-    
-//        var map = new google.maps.Map(document.getElementById('map'), {
-//            center: {lat: latitud, lng:  longitud},
-//            zoom: 18
-//        });
-
-
- 
-//        marker = new google.maps.Marker({
-//            map: map
-//        });
-
-
-
-
-//        if (!placeId) {
-//                return;
-//        }
-
-//        geocoder.geocode({ 'placeId': placeId }, function (results, status) {
-
-//                if (status !== 'OK') {
-//                    window.alert('Geocoder failed due to: ' + status);
-//                    return;
-//                }
-//                map.setZoom(18);
-//                map.setCenter(results[0].geometry.location);
-//                // Set the position of the marker using the place ID and location.
-
+    function cargarDatos(p_place,p_result) {
        
-                 
-//                marker.setPlace({
-//                    placeId: place.place_id,
-//                    location: results[0].geometry.location
-//                });
-//                marker.setVisible(true);
-//                document.getElementById('place-name').textContent = place.name;
-//                document.getElementById('place-id').textContent = place.place_id;
-//                document.getElementById('place-address').textContent =
-//                    results[0].formatted_address;
-//                infowindow.setContent(document.getElementById('infowindow-content'));
-//                infowindow.open(map, marker);
 
-//                alert(results[0].formatted_address);
-//                var preinf = place.name
-//                ubicacion = results[0].geometry.location;
-//                id_ubicacion = place.place_id;
 
-//            });
-//      }
+        var preinf = p_place.name
+        ubicacion = p_result[0].geometry.location.toString();
+        ubicacion = ubicacion.split('(');
+        ubicacion = ubicacion[1];
+        ubicacion = ubicacion.split(')');
+        ubicacion = ubicacion[0];
+        id_ubicacion = p_place.place_id;
+        nombre = p_place.name.split(',');
+        nombre = nombre[0];
+        direccion = preinf.substring(nombre.length + 2);
 
+          //En esta sesion se cargan los campos del formulario
+        document.getElementById('nombre').value = nombre;
+        document.getElementById('direccion').value = direccion;
+        document.getElementById('ubicacion').value = ubicacion;
+        document.getElementById('id_ubicacion').value = id_ubicacion;
+
+    }
